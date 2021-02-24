327b60e751b27259da8767258f8874d846729f7d
Gavin Fong <gavinklfong@gmail.com>
2021-02-24 17:00:02 +0000

    added readme

--- src/main/java/space/gavinklfong/forex/controllers/TradeDealRestController.java
+++ src/main/java/space/gavinklfong/forex/controllers/TradeDealRestController.java
@@ -29,17 +29,18 @@ public class TradeDealRestController {
 	@Autowired
 	private TradeService tradeService;
 	
-	
-	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public Mono<TradeDeal> submitDeal(@Valid @RequestBody Mono<TradeDealReq> req) {
-		
-		// submit trade deal
-		return req.flatMap(dealReq -> tradeService.postTradeDeal(dealReq));	
-	}
-	
+	/**
+	 * Expose API for customers to retrieve their forex trade deals
+	 * 
+	 * API - GET /deals
+	 * 
+	 * @param customerId. Exception will be thrown if customer id is empty, 
+	 * the exception will be translated to HTTP response with 4xx status
+	 * 
+	 * @return List of forex trade deal records. The framework formats it into JSON format when sending HTTP response
+	 */
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public Flux<TradeDeal> getDeals(@RequestParam Long customerId) {
-		
 		
 		if (customerId == null) {
 			return Flux.error(new InvalidRequestException("customerId", "customer Id cannot be blank"));
@@ -47,4 +48,26 @@ public class TradeDealRestController {
 				
 		return tradeService.retrieveTradeDealByCustomer(customerId);
 	}
+	
+	/**
+	 * Expose API for customers to post forex trade deal
+	 * 
+	 * API - POST /deals
+	 * 
+	 * @param req - Java bean contains trade deal information.
+	 * '@RequestBody' annotation instructs the framework to convert JSON format into Java bean
+	 * while '@Valid' annotation tells the framework to validate the object otherwise exception will be thrown
+	 * and the framework will send HTTP response with 4xx status back to client
+	 * 
+	 * 
+	 * @return Trade deal object. The framework formats it into JSON format when sending HTTP response
+	 */
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Mono<TradeDeal> submitDeal(@Valid @RequestBody Mono<TradeDealReq> req) {
+		
+		// submit trade deal
+		return req.flatMap(dealReq -> tradeService.postTradeDeal(dealReq));	
+	}
+	
+
 }
